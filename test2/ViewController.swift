8fc4d2263d646d9b7f26070d6a282b560bba5698
ahmetbostanciklioglu <40151328+ahmetbostan@users.noreply.github.com>
2022-04-21 23:02:52 +0300

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Ahmet Bostancıklıoğlu on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
